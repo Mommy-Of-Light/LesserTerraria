4b7e02545c76a3e1e6cf29bfdb59c40683eb580c
Mommy-Of-Light <empress.mommy.of.light@gmail.com>
2026-02-06 09:04:14 +0100

    Add project files.

---
+++ LesserTerraria/GlobalUsings.cs
@@ -0,0 +1,14 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Numerics;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Raylib_cs;
+
+global using static Raylib_cs.Raylib;
+global using static Raylib_cs.Color;
+
+global using static LesserTerraria.Constants;
---
+++ LesserTerraria/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace LesserTerraria
+{
+    public class Constants
+    {
+        public const int SCREEN_WIDTH = 800;
+        public const int SCREEN_HEIGHT = 450;
+
+        public const string GAME_TITLE = "Lesser Terraria";
+
+        public const int TARGET_FPS = 60;
+
+        public const KeyboardKey EXIT_KEY = KeyboardKey.Escape;
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+            SetTargetFPS(TARGET_FPS);
+            SetExitKey(EXIT_KEY);
+
+            Initialize();
+
+            while (!WindowShouldClose())
+            {
+                Update();
+                Draw();
+            }
+
+            CloseWindow();
+        }
+
+        static void Initialize()
+        {
+            // Initialization logic goes here
+        }
+
+        static void Update()
+        {
+            // Game update logic goes here
+        }
+
+        static void Draw()
+        {
+            #region Starting Drawing
+            BeginDrawing();
+            ClearBackground(White);
+            #endregion
+
+            #region Drawing Logic
+            DrawText("Hello raylib-cs!", 190, 200, 20, DarkGray);
+            #endregion
+
+            #region Ending Drawing
+            EndDrawing();
+            #endregion
+        }
+    }
+}
